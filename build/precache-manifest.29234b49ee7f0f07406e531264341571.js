b21e90fb03075b6fe0c4c2e50bbde4c53123300a
wsyou <wensiyou@bu.edu>
2019-02-26 18:36:21 -0500

    heroku

---
+++ build/precache-manifest.29234b49ee7f0f07406e531264341571.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f534c664a6e866597482",
+    "url": "/static/js/main.f534c664.chunk.js"
+  },
+  {
+    "revision": "d3949243597987ce8589",
+    "url": "/static/js/1.d3949243.chunk.js"
+  },
+  {
+    "revision": "f534c664a6e866597482",
+    "url": "/static/css/main.e8ba01b1.chunk.css"
+  },
+  {
+    "revision": "e970be9ffadaed9cf2a1a0385a384c25",
+    "url": "/index.html"
+  }
+];
