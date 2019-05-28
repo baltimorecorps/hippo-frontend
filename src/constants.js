17e592a03909959671f18d146ef214f6fc70158d
Merlin <merlinpatt@gmail.com>
2019-05-28 13:00:51 -0400

    Update API_URL

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,4 @@
-export const API_URL = 'http://127.0.0.1:5000';
+export const IS_LOCAL = window.location.href.includes('://localhost');
+export const API_URL = IS_LOCAL
+  ? 'http://127.0.0.1:5000'
+  : 'https://bcorps-hippo-backend.herokuapp.com/';
