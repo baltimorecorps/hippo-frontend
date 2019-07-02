d417a480a956f28b8de8f75aca5cb93ab854d3b8
Merlin <merlinpatt@gmail.com>
2019-07-02 16:30:20 -0400

    Switch to https

--- src/constants.js
+++ src/constants.js
@@ -1,9 +1,9 @@
 const getApiUrl = (environment) => {
   switch (environment) {
     case 'production':
-      return 'http://api.baltimorecorps.org/';
+      return 'https://api.baltimorecorps.org/';
     case 'staging':
-      return 'http://dev-api.baltimorecorps.org/';
+      return 'https://dev-api.baltimorecorps.org/';
     default:
       return 'http://127.0.0.1:5000/';
   }
