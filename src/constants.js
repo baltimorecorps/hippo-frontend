6d2bf2af7532e395e871b5773876f70425f7125a
Merlin <merlinpatt@gmail.com>
2019-07-02 13:14:43 -0400

    Update API_URL

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,12 @@
-export const IS_LOCAL = window.location.href.includes('://localhost');
-export const API_URL = IS_LOCAL
-  ? 'http://127.0.0.1:5000'
-  : 'https://bcorps-hippo-backend.herokuapp.com/';
+const getApiUrl = (environment) => {
+  switch (environment) {
+    case 'production':
+      return 'http://api.baltimorecorps.org/';
+    case 'staging':
+      return 'http://dev-api.baltimorecorps.org/';
+    default:
+      return 'http://127.0.0.1:5000/';
+  }
+};
+
+export const API_URL = getApiUrl(process.env.REACT_APP_ENVIRONMENT);
