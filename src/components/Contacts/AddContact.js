8ceae8e20a04453f322b1c8ead8f75008532e823
David Koh <david.f.koh@gmail.com>
2019-10-04 12:15:30 -0500

    Cleanup what we send to the api

--- src/components/Contacts/AddContact.js
+++ src/components/Contacts/AddContact.js
@@ -39,11 +39,13 @@ const useForm = (addNewContact) => {
   const [values, setValues] = useState({});
 
   const handleSubmit = () => {
-    values.email_primary = {
+    let submission = Object.assign({}, values)
+    submission.email_primary = {
       is_primary: true,
       email: values.email,
     };
-    addNewContact(values);
+    delete submission.email;
+    addNewContact(submission);
   };
 
   const handleChange = (event) => {
