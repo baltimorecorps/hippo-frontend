79e9b9162c19aa787d9d85e3172a1682b21ef713
David Koh <david.f.koh@gmail.com>
2019-10-25 13:03:34 -0500

    Pull resume selection out of state

--- src/pages/ProfilePage/ProfilePage.container.js
+++ src/pages/ProfilePage/ProfilePage.container.js
@@ -1,9 +1,11 @@
 import { connect } from 'react-redux';
+import { createSelector } from 'redux-starter-kit';
 import { refreshContacts } from 'actions/contacts';
 import {
   startResumeCreation,
   startResumeSelect,
   cancelResumeSelect,
+  generateResume,
 } from 'actions/resume';
 import { RESUME_CREATION } from 'reducers/resume';
 import ProfilePage from './ProfilePage';
@@ -13,6 +15,97 @@ const addNewContact = (dispatch) =>
   async function(contact) {
     await refreshContacts(dispatch);
   };
+
+// TODO: refactor to use generic selectors, merge with ExperiencesList.container
+const getExperiences = createSelector(
+  ['experiences'],
+  (experiences) => Object.keys(experiences).map((id) => experiences[id]),
+);
+
+const getSelection = createSelector(['resume.selected']);
+const getResumeCreationStep = createSelector(['resume.resumeCreationStep']);
+
+const getContact = (state, props) => props.match.params.contactId;
+
+const getResumeAll = createSelector(
+  [getExperiences, getSelection, getContact],
+  (exps, selection, contactId) => {
+    let resume = {
+      relevant_exp: [],
+      other_exp: [],
+      relevant_edu: [],
+      other_edu: [],
+      relevant_achieve: [],
+      other_achieve: [],
+      relevant_skills: [],
+      other_skills: [],
+    };
+    const contactExps = exps.filter(
+      (exp) => exp.contact_id.toString() === contactId.toString(),
+    );
+
+    [
+      ['Work', 'exp'],
+      ['Service', 'exp'],
+      ['Education', 'edu'],
+      ['Accomplishment', 'achieve'],
+    ].forEach(([type, suffix]) => {
+      contactExps
+        .filter((exp) => exp.type === type)
+        .forEach((exp) => { resume[`relevant_${suffix}`].push(exp.id); });
+    });
+    return resume;
+  },
+);
+
+const getResumeSelected = createSelector(
+  [getExperiences, getSelection, getContact],
+  (exps, selection, contactId) => {
+    let resume = {
+      relevant_exp: [],
+      other_exp: [],
+      relevant_edu: [],
+      other_edu: [],
+      relevant_achieve: [],
+      other_achieve: [],
+      relevant_skills: [],
+      other_skills: [],
+    };
+    const contactExps = exps.filter(
+      (exp) => exp.contact_id.toString() === contactId.toString(),
+    );
+
+    [
+      ['Work', 'exp', 'experience'],
+      ['Service', 'exp', 'experience'],
+      ['Education', 'edu', 'education'],
+      ['Accomplishment', 'achieve', 'accomplishments'],
+    ].forEach(([type, suffix, selectKey]) => {
+      contactExps
+        .filter((exp) => exp.type === type)
+        .forEach((exp) => {
+          if (selection[selectKey].indexOf(exp.id) !== -1) {
+            resume[`relevant_${suffix}`].push(exp.id);
+          } else {
+            resume[`other_${suffix}`].push(exp.id);
+          }
+        });
+    });
+    return resume;
+  },
+);
+
+const getResume = createSelector(
+  [getResumeCreationStep, getResumeSelected, getResumeAll],
+  (step, resumeSelected, resumeAll) => {
+    if (step === RESUME_CREATION.SELECT_HIGHLIGHTS) {
+      return resumeSelected;
+    } else {
+      return resumeAll;
+    }
+  }
+);
+
 
 // (DK 2019-09-12)
 // The state that the top level page uses is mostly the contact state,
@@ -28,7 +121,7 @@ const addNewContact = (dispatch) =>
 //
 // The props to this container specify which particular contact we want to
 // display on the page, and we pull that contact's info out of the state
-const mapStateToProps = (state, props) => {
+export const mapStateToProps = (state, props) => {
   const contactId = props.match.params.contactId;
   const contactInfo = state.contacts[contactId];
   return {
@@ -38,18 +131,21 @@ const mapStateToProps = (state, props) => {
       state.resume.resumeCreationStep === RESUME_CREATION.CHOOSE_STYLE,
     inSelectMode:
       state.resume.resumeCreationStep === RESUME_CREATION.SELECT_HIGHLIGHTS,
+    resume: getResume(state, props),
   };
 };
 
 // Refreshes the state of all contacts (as shown above) from the API, via
 // the ALL_CONTACTS event (see reducers/contacts.js for details)
-const mapDispatchToProps = (dispatch) => ({
+export const mapDispatchToProps = (dispatch) => ({
   refreshContacts: () => refreshContacts(dispatch),
   startResumeCreation: () => dispatch(startResumeCreation()),
   startResumeSelect: () => dispatch(startResumeSelect()),
   cancelResumeSelect: () => dispatch(cancelResumeSelect()),
-  generateResume: (contactId, resume) =>
-    generateResume(contactId, resume)(dispatch),
+  generateResume: (contactId, name, resume) => {
+    resume.name = name;
+    return generateResume(contactId, resume)(dispatch);
+  },
 });
 
 const ProfileContainer = connect(
