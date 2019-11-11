7154a071b3e464bdb348169e4bfd5ad6712add94
David Koh <david.f.koh@gmail.com>
2019-11-11 13:12:49 -0600

    Add auth library and login button

--- src/App.js
+++ src/App.js
@@ -2,16 +2,20 @@ import React from 'react';
 import {BrowserRouter as Router, Route, Switch, Link} from 'react-router-dom';
 import {MuiThemeProvider} from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
-import Icon from '@material-ui/core/Icon';
+import Button from '@material-ui/core/Button';
 import MenuItem from '@material-ui/core/MenuItem';
 import Toolbar from '@material-ui/core/Toolbar';
 import withStyles from '@material-ui/core/styles/withStyles';
+
+import HomeIcon from '@material-ui/icons/Home';
 
 import ErrorBoundary from 'atoms/ErrorBoundary';
 import Resume from 'modules/Resume';
 import CreateResumePage from 'pages/CreateResumePage';
 import ProfilePage from 'pages/ProfilePage';
 import theme from 'styles/theme';
+
+import {useAuth0} from 'lib/auth0';
 
 /* TODO remove these or move to pages folder */
 import Home from 'components/Other/Home.js';
@@ -20,69 +24,87 @@ import SearchContact from 'components/SearchContact/SearchContact';
 import ResumeTemplate from 'components/Other/ResumeTemplate';
 import TalentHome from 'components/TalentHome/TalentHome';
 
-const App = ({classes}) => (
-  <ErrorBoundary fileName="src/App.js">
-    <MuiThemeProvider theme={theme}>
-      <Router>
-        <div className={classes.page}>
-          <AppBar position="fixed">
-            <Toolbar>
-              <Link to="/">
-                <MenuItem>
-                  <Icon>home</Icon>
-                </MenuItem>
-              </Link>
-              <Link to="/resume">
-                <MenuItem>Generated Resume</MenuItem>
-              </Link>
-              <Link to="/create-resume">
-                <MenuItem>Create Resume</MenuItem>
-              </Link>
-              <Link to="/contacts">
-                <MenuItem>Contacts</MenuItem>
-              </Link>
-            </Toolbar>
-          </AppBar>
-          <Toolbar /> {/* for automatic padding of AppBar */}
-          <Switch>
-            <Route exact path="/" component={Home} />
+const App = ({classes}) => {
+  const {isAuthenticated, loginWithRedirect, logout} = useAuth0();
 
-            <Route exact path="/contacts" component={Contacts} />
-            <Route exact path="/search-contact" component={SearchContact} />
+  return (
+    <ErrorBoundary fileName="src/App.js">
+      <MuiThemeProvider theme={theme}>
+        <Router>
+          <div className={classes.page}>
+            <AppBar position="fixed">
+              <Toolbar>
+                <Link to="/">
+                  <MenuItem>
+                    <HomeIcon />
+                  </MenuItem>
+                </Link>
+                <Link to="/resume">
+                  <MenuItem>Generated Resume</MenuItem>
+                </Link>
+                <Link to="/create-resume">
+                  <MenuItem>Create Resume</MenuItem>
+                </Link>
+                <Link to="/contacts">
+                  <MenuItem>Contacts</MenuItem>
+                </Link>
+                <div className={classes.grow} />
+                {!isAuthenticated && (
+                  <Button color="inherit" onClick={() => loginWithRedirect({})}>
+                    Log in
+                  </Button>
+                )}
+                {isAuthenticated && (
+                  <Button color="inherit" onClick={() => logout()}>
+                    Log out
+                  </Button>
+                )}
+              </Toolbar>
+            </AppBar>
+            <Toolbar /> {/* for automatic padding of AppBar */}
+            <Switch>
+              <Route exact path="/" component={Home} />
 
-            <Route exact path="/resume/:gdocId" component={Resume} />
-            <Route exact path="/talent-home" component={TalentHome} />
+              <Route exact path="/contacts" component={Contacts} />
+              <Route exact path="/search-contact" component={SearchContact} />
 
-            <Route exact path="/profile/:contactId" component={ProfilePage} />
+              <Route exact path="/resume/:gdocId" component={Resume} />
+              <Route exact path="/talent-home" component={TalentHome} />
 
-            <Route
-              exact
-              path="/profile/:contactId/resume/:resumeId"
-              component={Resume}
-            />
-            <Route
-              exact
-              path="/contacts/:contactId/resume/:resumeId"
-              component={Resume}
-            />
-            <Route
-              exact
-              path="/contacts/:contactId/create-resume"
-              component={CreateResumePage}
-            />
-            <Route
-              exact
-              path="/contacts/:contactId/create-resume/:resumeId"
-              component={CreateResumePage}
-            />
-          </Switch>
-        </div>
-      </Router>
-    </MuiThemeProvider>
-  </ErrorBoundary>
-);
+              <Route exact path="/profile/:contactId" component={ProfilePage} />
+
+              <Route
+                exact
+                path="/profile/:contactId/resume/:resumeId"
+                component={Resume}
+              />
+              <Route
+                exact
+                path="/contacts/:contactId/resume/:resumeId"
+                component={Resume}
+              />
+              <Route
+                exact
+                path="/contacts/:contactId/create-resume"
+                component={CreateResumePage}
+              />
+              <Route
+                exact
+                path="/contacts/:contactId/create-resume/:resumeId"
+                component={CreateResumePage}
+              />
+            </Switch>
+          </div>
+        </Router>
+      </MuiThemeProvider>
+    </ErrorBoundary>
+  );
+};
 
 const styles = ({breakpoints, palette, spacing}) => ({
+  grow: {
+    flexGrow: 1,
+  },
   page: {
     backgroundColor: 'hsl(216, 18%, 89%)',
     paddingBottom: spacing(5),
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import {Provider} from 'react-redux';
 import {configureStore} from 'redux-starter-kit';
 import rootReducer from './reducers';
 import App from './App';
+import {Auth0Provider} from './lib/auth0';
+import config from './authConfig.json';
 
 import * as serviceWorker from './serviceWorker';
 
@@ -11,9 +13,30 @@ const store = configureStore({
   reducer: rootReducer,
 });
 
+// A function that routes the user to the right place
+// after login
+//
+// See: https://auth0.com/docs/quickstart/spa/react
+const onRedirectCallback = appState => {
+  window.history.replaceState(
+    {},
+    document.title,
+    appState && appState.targetUrl
+      ? appState.targetUrl
+      : window.location.pathname
+  );
+};
+
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <Auth0Provider
+      domain={config.domain}
+      client_id={config.clientId}
+      redirect_uri={window.location.origin}
+      onRedirectCallback={onRedirectCallback}
+    >
+      <App />
+    </Auth0Provider>
   </Provider>,
   document.getElementById('root')
 );
