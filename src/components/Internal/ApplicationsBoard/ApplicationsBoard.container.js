f52cee6ec7f52d32d6ead43cd6f864889e6c484a
BayLadyCoder <rachada_32@hotmail.com>
2020-06-26 17:53:36 -0400

    Load only approved applicants to render instead of all applicants to make it renders quicker

--- src/components/Internal/ApplicationsBoard/ApplicationsBoard.container.js
+++ src/components/Internal/ApplicationsBoard/ApplicationsBoard.container.js
@@ -1,24 +1,33 @@
 import {connect} from 'react-redux';
 import ApplicationsBoard from './ApplicationsBoard';
-import {getAllContactsPrograms, approveNewApplicants} from 'state/opportunity';
+import {
+  getAllContactsPrograms,
+  approveNewApplicants,
+  getAllApprovedApplicants,
+} from 'state/opportunity';
 
 const mapStateToProps = state => {
-  const allApplicants = Object.values(state.applicants);
-  const approvedApplicants = allApplicants.filter(app => {
-    const isApproved = app.programs.filter(
-      program => program.is_approved === true
-    );
-    return isApproved.length > 0;
-  });
+  const allApplicants = state.applicants['all_applicants'];
+  const approvedApplicants = state.applicants['approved_applicants'];
+  const unapprovedApplicants =
+    allApplicants &&
+    allApplicants.filter(app => {
+      const isApproved = app.programs.filter(program => {
+        return program.is_approved === false;
+      });
+      return isApproved.length > 0;
+    });
 
   return {
     allApplicants,
+    unapprovedApplicants,
     approvedApplicants,
   };
 };
 
 const mapDispatchToProps = dispatch => ({
   getAllContactsPrograms: () => getAllContactsPrograms(dispatch),
+  getAllApprovedApplicants: () => getAllApprovedApplicants(dispatch),
   approveNewApplicants: (programId, applicants) =>
     approveNewApplicants(programId, applicants)(dispatch),
 });
