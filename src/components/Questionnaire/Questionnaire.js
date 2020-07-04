20c75a01dfce832ab50eddddc8f3eabd68b84718
BayLadyCoder <rachada_32@hotmail.com>
2020-07-04 17:29:25 -0400

    Added Demographic Info form to Questionnaire

--- src/components/Questionnaire/Questionnaire.js
+++ src/components/Questionnaire/Questionnaire.js
@@ -29,6 +29,9 @@ import {
   jobSearchStatus,
   yearsOfExperience,
   roleLabels,
+  genders,
+  pronouns,
+  raceLabels,
 } from '../AboutMe/defaultData';
 
 import {
@@ -36,6 +39,8 @@ import {
   FormRadioButtons,
   FormCheckboxes,
   FormSubmitButton,
+  FormTextField,
+  FormDropDownSelector,
 } from '../AboutMe/forms/FormTemplates';
 
 const useForm = (initialValues, onSubmit) => {
@@ -66,6 +71,24 @@ const useForm = (initialValues, onSubmit) => {
       };
       update('interested_roles')(newValue);
     },
+    handleRacesChange: event => {
+      event.persist();
+      const newValue = {
+        ...values.race,
+        [event.target.name]: event.target.checked,
+      };
+      update('race')(newValue);
+    },
+
+    handleRaceOther: event => {
+      event.persist();
+      const newValue = {
+        ...values.race,
+        [event.target.name]: event.target.value,
+      };
+
+      update('race')(newValue);
+    },
   };
 
   return [values, handlers];
@@ -87,6 +110,8 @@ const Questionnaire = ({
 
       handleAddress,
       handleInterestedRolesChange,
+      handleRacesChange,
+      handleRaceOther,
     },
   ] = useForm(contact.profile, onSubmit);
   const [errors, setErrors] = useState({});
@@ -189,6 +214,19 @@ const Questionnaire = ({
       }
     });
   }
+  let race = [];
+  for (const [key, value] of Object.entries(values.race)) {
+    race.push({name: key, checked: value});
+  }
+
+  for (const [key, value] of Object.entries(raceLabels)) {
+    race.forEach((role, index) => {
+      if (role.name === key) {
+        race[index] = {...role, label: value};
+      }
+    });
+  }
+  const raceOptions = race.slice(0, -1);
 
   return (
     <Paper className={classes.paper}>
@@ -329,8 +367,83 @@ const Questionnaire = ({
               ariaLabel="Have participated with Baltimore Corps programs and services before"
             />
           </fieldset>
+          <fieldset className={classes.sectionContainer}>
+            <legend>
+              <Typography
+                variant="body1"
+                component="p"
+                className={classes.sectionHeader}
+              >
+                Demographic Information
+              </Typography>
+            </legend>
+            <Typography
+              variant="body1"
+              component="p"
+              className={classes.sectionDescription}
+            >
+              The information below helps us build a better picture of our
+              applicants. As an organization committed to equity, it is
+              important for us to understand the variety of identities and
+              affinities that are represented within our pool so that we can
+              engage in a thoughtful process. That being said, we understand
+              that this information is sensitive and providing it is completely
+              optional.
+            </Typography>
+            <FormCheckboxes
+              question="Race (select all that apply)"
+              options={raceOptions}
+              onChange={handleRacesChange}
+            />
+            {values.race.not_listed && (
+              <div className={classes.otherRace}>
+                <FormTextField
+                  value={values.race.race_other}
+                  name="race_other"
+                  label="We understand that the options listed above are not exhaustive. If your identity is not listed above, please let us know how you identify:"
+                  onChange={handleRaceOther}
+                />
+              </div>
+            )}
+            <div className={classes.genderAndPronounsContainer}>
+              <div className={classes.dropdownAndTextFieldContainer}>
+                <FormDropDownSelector
+                  question="Gender"
+                  name="gender"
+                  value={values.gender}
+                  options={genders}
+                  onChange={handleChange}
+                />
 
-          {/* </div> */}
+                {values.gender === 'Not Listed' && (
+                  <FormTextField
+                    value={values.gender_other}
+                    name="gender_other"
+                    label=" We understand that the options provided above are limited. If your gender identity is not listed above, please let us know how you identify:"
+                    onChange={handleChange}
+                  />
+                )}
+              </div>
+              <div className={classes.dropdownAndTextFieldContainer}>
+                <FormDropDownSelector
+                  question="Pronoun"
+                  name="pronoun"
+                  value={values.pronoun}
+                  options={pronouns}
+                  onChange={handleChange}
+                />
+
+                {values.pronoun === 'Not Listed' && (
+                  <FormTextField
+                    value={values.pronoun_other}
+                    name="pronoun_other"
+                    label="We understand that the options listed above are not exhaustive. If you use a set of pronouns that aren't listed above, please let us know what they are:"
+                    onChange={handleChange}
+                  />
+                )}
+              </div>
+            </div>
+          </fieldset>
         </form>
       </Grid>
     </Paper>
