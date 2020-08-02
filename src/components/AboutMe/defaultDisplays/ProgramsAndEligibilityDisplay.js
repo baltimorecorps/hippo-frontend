b4452e5450841325a6fec1f4559dcd15d4fccd83
BayLadyCoder <rachada_32@hotmail.com>
2020-08-02 03:52:08 -0400

    Refactor ProgramsDisplay using lodash.get

--- src/components/AboutMe/defaultDisplays/ProgramsAndEligibilityDisplay.js
+++ src/components/AboutMe/defaultDisplays/ProgramsAndEligibilityDisplay.js
@@ -4,14 +4,10 @@ import PropTypes from 'prop-types';
 import {
   Header,
   QuestionWithMultipleAnswers,
-  QuestionWithOneAnswer,
 } from './QuestionAnswerDisplayTemplates.js';
-import {blankProfile} from '../defaultData';
 import get from 'lodash.get';
 
 const ProgramsAndEligibilityDisplay = ({contact, onClickEdit}) => {
-  const profile = get(contact, 'profile', blankProfile);
-  console.log('profile', profile);
   const needs_help_programs = get(
     contact,
     'profile.needs_help_programs',
@@ -24,23 +20,14 @@ const ProgramsAndEligibilityDisplay = ({contact, onClickEdit}) => {
 
   if (needs_help_programs === true)
     checkedPrograms.push("I'd like some help figuring this out");
+
   return (
     <React.Fragment>
       <Header header="Programs and Eligibility" onClickEdit={onClickEdit} />
-
-      {contact.profile &&
-      contact.profile.needs_help_programs === true &&
-      checkedPrograms.length === 0 ? (
-        <QuestionWithOneAnswer
-          question="Interested Programs:"
-          answer="I'd like some help figuring this out"
-        />
-      ) : (
-        <QuestionWithMultipleAnswers
-          question="Interested Programs:"
-          answers={checkedPrograms}
-        />
-      )}
+      <QuestionWithMultipleAnswers
+        question="Interested Programs:"
+        answers={checkedPrograms}
+      />
     </React.Fragment>
   );
 };
