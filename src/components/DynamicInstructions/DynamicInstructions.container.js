ac27ea0c22e136bf714c0f96bb25d193bdb70820
BayLadyCoder <rachada_32@hotmail.com>
2020-08-18 17:11:59 -0400

    Quick fix on loading profile data from other pages and create new profile when profile is null

--- src/components/DynamicInstructions/DynamicInstructions.container.js
+++ src/components/DynamicInstructions/DynamicInstructions.container.js
@@ -1,22 +1,8 @@
 import {connect} from 'react-redux';
 import {getDynamicInstructions, submitProfileForReview} from 'state/contacts';
 import DynamicInstructions from './DynamicInstructions';
-import {blankInstructions} from './defaultValues';
 
-import get from 'lodash.get';
-
-export const mapStateToProps = state => {
-  const contact = Object.values(state.contacts)[0];
-
-  const id = get(contact, 'id', null);
-  const instructions = get(contact, 'instructions', blankInstructions);
-  const status = get(contact, 'status', 'created');
-  return {
-    id,
-    instructions,
-    status,
-  };
-};
+export const mapStateToProps = state => {};
 
 export const mapDispatchToProps = dispatch => ({
   getDynamicInstructions: contactId =>
