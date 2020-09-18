b2e2b7ca1bd4540e8fe4f78822e6e300c0f2ec55
BayLadyCoder <rachada_32@hotmail.com>
2020-09-18 13:47:17 -0400

    Changed wording to Finalists for Roles instead of Considered for Roles on ApplicantPage

--- src/components/Internal/ApplicantsBoard/ApplicationsCard.container.js
+++ src/components/Internal/ApplicantsBoard/ApplicationsCard.container.js
@@ -1,29 +1,29 @@
-import {connect} from 'react-redux';
-import ApplicationsCard from './ApplicationsCard';
-import {getContactApplications} from 'state/opportunity';
-import {getContact} from 'state/contacts';
+// import {connect} from 'react-redux';
+// import ApplicationsCard from './ApplicationsCard';
+// import {getContactApplications} from 'state/opportunity';
+// import {getContact} from 'state/contacts';
 
-const mapStateToProps = (state, props) => {
-  const {contactId} = props.match.params;
+// const mapStateToProps = (state, props) => {
+//   const {contactId} = props.match.params;
 
-  const applications = Object.values(state.applications).filter(
-    app => app.contact.id === Number(contactId)
-  );
-  const applicant = Object.values(state.contacts).filter(
-    contact => contact.id === Number(contactId)
-  );
+//   const applications = Object.values(state.applications).filter(
+//     app => app.contact.id === Number(contactId)
+//   );
+//   const applicant = Object.values(state.contacts).filter(
+//     contact => contact.id === Number(contactId)
+//   );
 
-  return {
-    applications: applications,
-    applicant: applicant[0],
-    contactId: Number(contactId),
-  };
-};
+//   return {
+//     applications: applications,
+//     applicant: applicant[0],
+//     contactId: Number(contactId),
+//   };
+// };
 
-const mapDispatchToProps = dispatch => ({
-  getContactApplications: contactId =>
-    getContactApplications(contactId)(dispatch),
-  getContact: contactId => getContact(contactId)(dispatch),
-});
+// const mapDispatchToProps = dispatch => ({
+//   getContactApplications: contactId =>
+//     getContactApplications(contactId)(dispatch),
+//   getContact: contactId => getContact(contactId)(dispatch),
+// });
 
-export default connect(mapStateToProps, mapDispatchToProps)(ApplicationsCard);
+// export default connect(mapStateToProps, mapDispatchToProps)(ApplicationsCard);
