6fdba84514410e06a7dd2d4178e8b56a363b8e71
BayLadyCoder <rachada_32@hotmail.com>
2020-10-08 13:32:02 -0400

    Renamed /Footer to /MainFooter

--- src/app/App.js
+++ src/app/App.js
@@ -9,7 +9,7 @@ import MenuItem from '@material-ui/core/MenuItem';
 import Toolbar from '@material-ui/core/Toolbar';
 import withStyles from '@material-ui/core/styles/withStyles';
 import Typography from '@material-ui/core/Typography';
-import Footer from 'components/Footer';
+import Footer from 'components/MainFooter';
 import NavBarIcons from 'components/NavigationBar/NavBarIcons';
 import HomeIcon from '@material-ui/icons/Home';
 
