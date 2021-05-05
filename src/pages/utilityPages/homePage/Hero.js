c258f000db9d53b6cc9316e23acf89e58b26b462
vishalicious213 <vishalicious213@gmail.com>
2021-05-05 13:47:02 -0700

    Hero.js - added Grid, Typographies & Button

--- src/pages/utilityPages/homePage/Hero.js
+++ src/pages/utilityPages/homePage/Hero.js
@@ -5,25 +5,30 @@ import Typography from '@material-ui/core/Typography'
 
 function Hero() {
     return (
-        <main>
-            <Typography color="inherit" align="center" variant="h2" marked="center">
-                PLACE FOR PURPOSE
-            </Typography>
-            <Typography color="inherit" align="center" variant="h5">
-                is a service that connects talented community members facing challenges finding employment in the social impact sector with organizations that have taken an internal and external commitment to equity and racial justice.
-            </Typography>
-            <Typography color="inherit" align="center" variant="h5">
-            Create an account or log in to create a Baltimore Corps community profile. Get access to job opportunities and development opportunities in the Baltimore Corps network.
-            </Typography>
-            <Button
-                variant="contained"
-                color="primary"
-                size="large"
-                // onClick={onClickLogInHandler}
-                >
-                Log in / Sign up
-            </Button>
-        </main>
+        <Grid container>
+            <Grid item>
+                <Typography color="inherit" align="center" variant="h2">
+                    PLACE FOR PURPOSE
+                </Typography>
+
+                <Typography color="inherit" align="center" variant="h5">
+                    is a service that connects talented community members facing challenges finding employment in the social impact sector with organizations that have taken an internal and external commitment to equity and racial justice.
+                </Typography>
+
+                <Typography color="inherit" align="center" variant="h5">
+                Create an account or log in to create a Baltimore Corps community profile. Get access to job opportunities and development opportunities in the Baltimore Corps network.
+                </Typography>
+
+                <Button
+                    variant="contained"
+                    color="primary"
+                    size="large"
+                    // onClick={onClickLogInHandler}
+                    >
+                    Log in / Sign up
+                </Button>
+            </Grid>
+        </Grid>
     )
 }
 
