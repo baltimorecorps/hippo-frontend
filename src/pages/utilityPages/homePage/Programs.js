33d8c12d41e4d53138e6957d8e30791d117bd7e6
vishalicious213 <vishalicious213@gmail.com>
2021-05-05 15:53:48 -0700

    Programs - setting up layout

--- src/pages/utilityPages/homePage/Programs.js
+++ src/pages/utilityPages/homePage/Programs.js
@@ -7,30 +7,27 @@ import Box from '@material-ui/core/Box'
 function Programs() {
     return (
         <Box width={"100%"} mt={3}>
+            <Typography color="inherit" align="center" gutterBottom variant="h3" component="h3">
+                OUR PROGRAMS
+            </Typography>
+
             <Grid container justify="center">
-                <Grid item xs={10} sm={10} md={8} lg={6} xl={4}>
-                    <Typography color="inherit" align="center" gutterBottom variant="h3" component="h3">
-                        OUR PROGRAMS
+                <Grid item xs={9} md={3}>
+                    <Typography color="inherit" align="center" gutterbottom>
+                        1
                     </Typography>
+                </Grid>
 
-                    {/* <Typography color="inherit" align="center" gutterBottom variant="body1" component="p">
-                        is a service that connects talented community members facing challenges finding employment in the social impact sector with organizations that have taken an internal and external commitment to equity and racial justice.
+                <Grid item xs={9} md={3}>
+                    <Typography color="inherit" align="center" gutterbottom>
+                        2
                     </Typography>
+                </Grid>
 
-                    <Typography color="inherit" align="center" gutterBottom variant="body2" component="p">
-                        Create an account or log in to create a Baltimore Corps community profile. Get access to job opportunities and development opportunities in the Baltimore Corps network.
+                <Grid item xs={9} md={3}>
+                    <Typography color="inherit" align="center" gutterbottom>
+                        3
                     </Typography>
-
-                    <Box align="center" mt={2}>
-                        <Button
-                            variant="contained"
-                            color="primary"
-                            size="large"
-                            // onClick={onClickLogInHandler}
-                            >
-                            Log in / Sign up
-                        </Button>
-                    </Box> */}
                 </Grid>
             </Grid>
         </Box>
