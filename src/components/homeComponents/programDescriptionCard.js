456c2022312b79ef434bfa3b4166c99f7029dd76
vishalicious213 <vishalicious213@gmail.com>
2021-05-07 13:55:03 -0700

    programDescriptionCard: styled text

--- src/components/homeComponents/programDescriptionCard.js
+++ src/components/homeComponents/programDescriptionCard.js
@@ -1,6 +1,23 @@
 import React from 'react'
 import Typography from '@material-ui/core/Typography'
 
+const styles = {
+    title: {
+        fontSize: "1rem",
+        lineHeight: "25px",
+        textTransform: "uppercase"
+    },
+    underline: {
+        width: "50%",
+        minWidth: "25rem",
+        borderTop: "2px solid #ffcc33"
+    },
+    desc: {
+        width: "80%",
+        margin: "1rem 0 2rem 0"
+    }
+}
+
 function ProgramDescriptionCard(props) {
     return (
         <>
@@ -8,16 +25,18 @@ function ProgramDescriptionCard(props) {
             gutterBottom
             variant="h4"
             component="h1"
-            // className={classes.pageHeader}
+            style={styles.title}
+            color="primary"
         >                    
             {props.title}
         </Typography>
+        <hr style={styles.underline} />
         <Typography
             gutterBottom
             // className={classes.bcCopy}
             variant="body1"
             component="p"
-            style={{ maxWidth:'80%'}}
+            style={styles.desc}
         >                    
             {props.description}
         </Typography>
