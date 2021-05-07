5868ec4b042e6e7882c4623f1e50c3a1cb5d3b5c
vishalicious213 <vishalicious213@gmail.com>
2021-05-06 20:39:48 -0700

    P4PCarousel - set breakpoints so cards don't colllide when screen width changes

--- src/components/homeComponents/P4PCarousel.js
+++ src/components/homeComponents/P4PCarousel.js
@@ -101,17 +101,17 @@ const data = [
 function P4PCarousel({props, onClickLogInHandler}) {
     const responsive = {
         desktop: {
-          breakpoint: { max: 3000, min: 1024 },
+          breakpoint: { max: 3000, min: 1151 },
           items: 3,
           slidesToSlide: 3 // optional, default to 1.
         },
         tablet: {
-          breakpoint: { max: 1024, min: 464 },
+          breakpoint: { max: 1150, min: 769 },
           items: 2,
           slidesToSlide: 2 // optional, default to 1.
         },
         mobile: {
-          breakpoint: { max: 464, min: 0 },
+          breakpoint: { max: 768, min: 0 },
           items: 1,
           slidesToSlide: 1 // optional, default to 1.
         }
